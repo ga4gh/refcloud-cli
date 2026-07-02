8f445d97078d06e947988f478dda57f5f7f49c81
Jeremy Adams <jeremy.adams@ga4gh.org>
2026-07-02 13:13:54 -0400

    register manifest object in database

--- src/refcloud/custom_onboarding/onboard_1000_genomes.py
+++ src/refcloud/custom_onboarding/onboard_1000_genomes.py
@@ -1,6 +1,8 @@
 import fnmatch
 import os
 import time
+import json
+import hashlib
 
 
 REGION = "us-east-1"
@@ -67,7 +69,27 @@ def get_subfolders_page(s3_client, bucket_name, directory_prefix, continuation_t
 
 
 def process_subject_level_exome_directory(s3_client, exome_directory):
-    drs_object_set = {}
+    drs_object_set = {
+        "manifest_obj": {
+            "drs_object": {
+                "id": None,
+                "description": None,
+                "created_time": None,
+                "mime_type": "application/octet-stream",
+                "name": None,
+                "size": None,
+                "updated_time": None,
+                "version": "v1",
+                "is_manifest": True,
+                "manifest_content": ""
+            },
+            "aliases": [],
+            "checksums": [],
+            "aws_s3_access_objects": []
+        },
+        "file_keys": [],
+        "file_objs": {}
+    }
 
     kwargs = {
         "Bucket": BUCKET_NAME,
@@ -88,7 +110,7 @@ def process_subject_level_exome_directory(s3_client, exome_directory):
                 # reusable metadata components
                 subject = file_name.split(".")[0]
                 suffix = obj['Key'].split(".")[-1]
-                mainfest_key = suffix + "_file"
+                file_key = suffix + "_file"
 
                 # capture all metadata required for DRS
                 drs_object_metadata = {
@@ -121,8 +143,26 @@ def process_subject_level_exome_directory(s3_client, exome_directory):
                 }
 
                 # assign file according to file type so it can be tracked in the manifest 
-                
-                drs_object_set[mainfest_key] = drs_object_metadata
+                drs_object_set["file_keys"].append(file_key)
+                drs_object_set["file_objs"][file_key] = drs_object_metadata
+
+                if suffix == "bam":
+                    drs_object_set["manifest_obj"]["drs_object"]["id"] = f"{file_name}.manifest",
+                    drs_object_set["manifest_obj"]["drs_object"]["description"] = f"{subject} whole-exome file manifest",
+                    drs_object_set["manifest_obj"]["drs_object"]["created_time"] = obj['LastModified'].strftime("%Y-%m-%d %H:%M:%S")
+                    drs_object_set["manifest_obj"]["drs_object"]["name"] = f"{file_name}.manifest",
+                    drs_object_set["manifest_obj"]["drs_object"]["updated_time"] = obj['LastModified'].strftime("%Y-%m-%d %H:%M:%S")
+                    drs_object_set["manifest_obj"]["aliases"].append(f"{subject} whole-exome file manifest")
+
+    # finalize manifest metadata
+    manifest_content_obj = {key: drs_object_set["file_objs"][key]["drs_object"]["id"] for key in drs_object_set["file_keys"]}
+    manifest_content_str = json.dumps(manifest_content_obj)
+    manifest_size = len(manifest_content_str.encode('utf-8'))
+    manifest_md5 = hashlib.md5(manifest_content_str.encode('utf-8')).hexdigest()
+
+    drs_object_set["manifest_obj"]["drs_object"]["manifest_content"] = manifest_content_obj
+    drs_object_set["manifest_obj"]["drs_object"]["size"] = manifest_size
+    drs_object_set["manifest_obj"]["checksums"].append({"checksum": manifest_md5, "type": "md5"})
 
     print(f" {exome_directory} total files: {tc} matched files: {mc}")
     time.sleep(THROTTLE_TIME_SECS) # throttle requests so client doesn't get blocked by S3
--- src/refcloud/handlers/dataset/onboard.py
+++ src/refcloud/handlers/dataset/onboard.py
@@ -6,6 +6,7 @@ from refcloud.config.database_engine import DatabaseEngine
 from refcloud.config.constants import SUPPORTED_DATASETS
 from operator import itemgetter
 
+
 def dataset_onboard(dataset_id):
     database_engine = DatabaseEngine()
 
@@ -25,26 +26,27 @@ def dataset_onboard(dataset_id):
         onboarding_fn = data_source["s3info"]["custom_onboarding_function"]
 
         for drs_object_set in onboarding_fn(s3_client): # load drs objects and sub-objects (aliases, checksums, aws s3 access objects) into db
+            if len(drs_object_set['file_keys']) > 0:
+                all_meta_objects = [drs_object_set['file_objs'][file_key] for file_key in drs_object_set['file_keys']] + [drs_object_set["manifest_obj"]]
+                for meta_obj in all_meta_objects:
+                    # unpack
+                    drs_object, aliases, checksums, aws_s3_access_objects = itemgetter("drs_object", "aliases", "checksums", "aws_s3_access_objects")(meta_obj)
 
-            for manifest_key in drs_object_set.keys():
-                # unpack
-                drs_object, aliases, checksums, aws_s3_access_objects = itemgetter("drs_object", "aliases", "checksums", "aws_s3_access_objects")(drs_object_set[manifest_key])
+                    # add drs object
+                    drs_object["dataset_id"] = dataset_id
+                    database_engine.insert_drs_object(**drs_object)
 
-                # add drs object
-                drs_object["dataset_id"] = dataset_id
-                database_engine.insert_drs_object(**drs_object)
+                    # add aliases
+                    for alias in aliases:
+                        alias_obj = {"drs_object_id": drs_object["id"], "alias":alias}
+                        database_engine.insert_drs_object_alias(**alias_obj)
 
-                # add aliases
-                for alias in aliases:
-                    alias_obj = {"drs_object_id": drs_object["id"], "alias":alias}
-                    database_engine.insert_drs_object_alias(**alias_obj)
+                    # add checksums
+                    for checksum in checksums:
+                        checksum["drs_object_id"] = drs_object["id"]
+                        database_engine.insert_drs_object_checksum(**checksum)
 
-                # add checksums
-                for checksum in checksums:
-                    checksum["drs_object_id"] = drs_object["id"]
-                    database_engine.insert_drs_object_checksum(**checksum)
-
-                # add aws s3 access object
-                for aws_s3_access_object in aws_s3_access_objects:
-                    aws_s3_access_object["drs_object_id"] = drs_object["id"]
-                    database_engine.insert_aws_s3_access_object(**aws_s3_access_object)
+                    # add aws s3 access object
+                    for aws_s3_access_object in aws_s3_access_objects:
+                        aws_s3_access_object["drs_object_id"] = drs_object["id"]
+                        database_engine.insert_aws_s3_access_object(**aws_s3_access_object)
